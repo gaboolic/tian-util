5f1f2bd55dc997cec3aa4786e64ade412a75c538
23441099_m <23441099@qq.com>
2015-12-23 11:36:16 +0800

    httpUtil

--- src/main/java/tk/gbl/util/http/HttpUtil.java
+++ src/main/java/tk/gbl/util/http/HttpUtil.java
@@ -58,6 +58,8 @@ public class HttpUtil {
   public static String post(String url, String body, String contentType, String charset, String cookie) throws IOException {
     PostMethod method = new PostMethod(url);
     method.setRequestHeader("Cookie", cookie);
+    method.setRequestHeader("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36");
+    //method.setRequestHeader("X-Requested-With","ShockwaveFlash/18.0.0.232");
     method.setRequestEntity(new StringRequestEntity(body, contentType, charset));
     return post(method);
   }
@@ -73,6 +75,11 @@ public class HttpUtil {
 
   public static String post(String url, String body) throws IOException {
     return post(url, body, "application/x-www-form-urlencoded", "utf-8");
+  }
+
+  public static String post(String url, Map<String, Object> map) throws IOException {
+    String param = ParamUtil.genParam(map);
+    return post(url, param);
   }
 
   public static String postWithCookie(String url, String body, String cookie) throws IOException {
