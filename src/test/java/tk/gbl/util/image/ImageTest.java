590bacd2d3e3817fcd5f18d25100e366dad8eed9
23441099_m <23441099@qq.com>
2016-01-05 10:46:21 +0800

    图像处理

--- src/test/java/tk/gbl/util/image/ImageTest.java
+++ src/test/java/tk/gbl/util/image/ImageTest.java
@@ -1,5 +1,10 @@
 package tk.gbl.util.image;
 
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Date: 2016/1/5
  * Time: 10:43
@@ -7,4 +12,9 @@ package tk.gbl.util.image;
  * @author Tian.Dong
  */
 public class ImageTest {
+  @Test
+  public void test() throws IOException {
+    int[][] img = Binary.deal(new File("C:\\Users\\tian\\Desktop\\1-首页.png"));
+    ArrayToImage.createImage(img,new File("C:\\Users\\tian\\Desktop\\1-首页222.png"));
+  }
 }
