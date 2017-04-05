b9bc721b8dbb4bb6b8738d95af4c7fc9766ddeb8
tian.dong <tian.dong@corp.elong.com>
2017-04-05 19:34:57 +0800

    HttpClientUtil

--- src/main/java/tk/gbl/util/http/HttpClientUtil.java
+++ src/main/java/tk/gbl/util/http/HttpClientUtil.java
@@ -1,6 +1,8 @@
 package tk.gbl.util.http;
 
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.cookie.CookiePolicy;
+import org.apache.commons.httpclient.params.HttpMethodParams;
 
 /**
  * Created by tian on 2015/9/18.
@@ -11,6 +13,8 @@ public class HttpClientUtil {
     int time = 5000;
     httpClient.getHttpConnectionManager().getParams().setConnectionTimeout(time);
     httpClient.getHttpConnectionManager().getParams().setSoTimeout(time);
+    httpClient.getParams().setParameter(HttpMethodParams.HTTP_CONTENT_CHARSET, "UTF-8");
+    httpClient.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);
     return httpClient;
   }
 }
